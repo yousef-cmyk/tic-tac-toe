47b5b10a9535b9e08abaedf0b09e9156912d0c8a
unknown <yousef.ismail.akareb1elprince1@gmail.com>
2024-07-24 08:14:02 +0300

    player class done el7

--- tic tac toe/player.py
+++ tic tac toe/player.py
@@ -1,6 +1,20 @@
 class Player:
-    def __init__(self,name,symbol) :
-        self.__name=''
-        self.__symbol=''
+    def __init__(self) :
+        self.name=''
+        self.symbol=''
     def chooseName(self):
-        name=input('enter your name')
+        while true:
+            name=input('enter your name')
+            if name.isalpha():
+                self.name=name
+                break
+            else print('name can contain only letters')
+    def chooseSymbol(self):
+        while true:
+            symbol=input('enter your symbol')
+            if symbol.isalpha() and len(symbol)==1:
+                self.symbol==symbol.upper()
+                break
+            print("symbol must be only one letter")
+
+
